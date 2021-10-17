0566960725c6fe29a632ddd3ef41db4a34f36bb4
HabibIbrahimov <habibyi@code.edu.az>
2021-10-17 22:54:48 +0400

    added project

---
+++ KelvinAndCelsius/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KelvinAndCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celsius celsius = new Celsius(10);
+            Kelvin kelvin = celsius;
+            Console.WriteLine(kelvin.Kel);
+
+        }
+    }
+}
---
+++ KelvinAndCelsius/Temp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KelvinAndCelsius
+{
+    class Celsius
+    {
+        public double Cel { get; set; }
+        public Celsius(double c)
+        {
+            Cel = c;
+        }
+        public static implicit operator Kelvin(Celsius celsius)
+        {
+            return new Kelvin(celsius.Cel + 273.15);
+        }
+    }
+    class Kelvin
+    {
+        public double Kel { get; set; }
+        public Kelvin(double k)
+        {
+            Kel = k;
+        }
+    }
+}
